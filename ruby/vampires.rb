a9a22f71bf9aedb22b80f63ef5eef8399b12e52e
ikn02010 <ikn02010@gmail.com>
2017-08-09 12:01:22 -0700

    complete if else statements

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -12,3 +12,20 @@ bread = gets.chomp
 
 puts "Would you like to enroll in the company's health insurance?: "
 insurance = gets.chomp
+
+if (birthyear.to_i-2017 == age.to_i) && bread == "yes" || insurance == "yes"
+    if name == "Drake Cula" || name == "Tu Fang"
+      puts "Definitely a vampire"
+    elsif
+      puts "Probably not a vampire"
+    end
+elsif (birthyear.to_i-2017 != age.to_i) && bread == "no" ||insurance == "no"
+  puts "Probably a vampire"
+
+elsif (birthyear.to_i-2017 != age.to_i) && bread == "no" &&insurance == "no"
+  puts "Almost certainly a vampire"
+
+else
+  puts "Results inconclusive"
+end
+
