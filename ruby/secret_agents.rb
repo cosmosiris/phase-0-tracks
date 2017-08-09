d5eb84e08cd82bf47cf389c17f337a0f2f7bc6cf
ikn02010 <ikn02010@gmail.com>
2017-08-09 13:52:59 -0700

    complete decrypt method

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -21,6 +21,32 @@ end
 
 encrypt("abc")
 
+Define a method "decrypt" with parameter x
+setup a loop starting with an i that equals 0
+Create two variables, an alphabet variable with the alphabet in it and a string variable that is empty
+Setup a while loop where i is less than the length of x
+Set a new variable (new_var) equal to alphabet.index( )
+Then inside the parenthesis add index i of x
+Then set the string variable to add a new letter that is 1 index less than it originally was
+Then set the loop counter to increase by 1 at the end of the while loop
+Then end the while loop
+Then set string to print using puts
+Then end the method
+Then call the method
+
+def decrypt(x)
+  i = 0
+  alphabet = "abcdefghijklmnopqrstuvwxyz"
+  string = ""
+  while i < x.length
+    new_var = alphabet.index(x[i])
+    string += alphabet[new_var - 1]
+    i += 1
+  end
+  puts string
+end
+
+decrypt("abcdefgh")
 
 
 
