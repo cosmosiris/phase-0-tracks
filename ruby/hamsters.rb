07f97c3718104e1de5014f26dc02144fda441bfb
ikn02010 <ikn02010@gmail.com>
2017-08-12 17:12:07 -0700

    update program to print answers to the console

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -3,13 +3,18 @@
   hamstername = gets.chomp
 
   puts "What's the hamster's volume level?"
-  volume = gets.chomp
+  volume = gets.chomp.to_i
 
   puts "What's the hamster's fur color?"
   furcolor = gets.chomp
 
   puts "Is the hamster a good candidate for adoption?"
   adoption = gets.chomp
+    if adoption.downcase == "yes"
+      adoption = "is"
+    else
+      adoption = "is not"
+    end
 
   puts "Estimated age?"
   estimatedage = gets.chomp
@@ -19,5 +24,6 @@
       estimatedage = estimatedage.to_i
     end
 
-puts estimatedage
+puts "This hamster's name is #{hamstername}, its volume level is #{volume}, and its fur color is #{furcolor}. It is estimated to be #{estimatedage} years old and #{adoption} suitable for adoption."
+
 
