c53311bfb69d855feaf7f5c38cfbcc3edd0ee74f
ikn02010 <ikn02010@gmail.com>
2017-08-14 10:57:15 -0700

    complete assignment

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -4,36 +4,53 @@
 # When there's more than one suspect who could have
 # committed the crime, add additional calls to prove it.
 
-# "iNvEsTiGaTiOn".<???>
+p "iNvEsTiGaTiOn".swapcase
 # => “InVeStIgAtIoN”
 
-# "zom".<???>
+p "zom".insert(1, "o")
+p "zom"[0,3] = "zoom"
+p "zom".sub!("o", "oo")
 # => “zoom”
 
-# "enhance".<???>
+p "enhance".center(12)
 # => "    enhance    "
 
-# "Stop! You’re under arrest!".<???>
+p "Stop! You’re under arrest!".upcase
 # => "STOP! YOU’RE UNDER ARREST!"
 
-# "the usual".<???>
+p "the usual" + " suspects"
+p "the usual" << " suspects"
+p "the usual".insert(9," suspects")
+p "the usual".insert(9," suspects")
 #=> "the usual suspects"
 
-# " suspects".<???>
+p " suspects".lstrip
+p " suspects".lstrip!
+p " suspects".strip
+p " suspects".strip!
 # => "the usual suspects"
 
-# "The case of the disappearing last letter".<???>
+p "The case of the disappearing last letter"["The case of the disappearing last lette"]
+p "The case of the disappearing last letter".chop
+p "The case of the disappearing last letter"[0,39]
 # => "The case of the disappearing last lette"
 
-# "The mystery of the missing first letter".<???>
+p "The mystery of the missing first letter"["he mystery of the missing first letter"]
+p "The mystery of the missing first letter"[1, 40]
+p "The mystery of the missing first letter"[0] = ""
 # => "he mystery of the missing first letter"
 
-# "Elementary,    my   dear        Watson!".<???>
+p "Elementary,    my   dear        Watson!"[0, 38] = "Elementary, my dear Watson!"
+p "Elementary,    my   dear        Watson!".gsub(/\s+/, " ")
+p "Elementary,    my   dear        Watson!".squeeze(" ")
+p "Elementary,    my   dear        Watson!".split.join(" ")
 # => "Elementary, my dear Watson!"
 
-# "z".<???>
-# => 122 
+p "z".gsub("z", "122").to_i
+p "z".codepoints
+p "The unicode system defines a unique codepoint for each character in a multitude of world languages, scientific symbols, currencies etc. The codepoints for the english alphabet begin at 97, meaning the codepoint for 'a' is 97, for 'b', is 98, for 'c', is 99, etc"
+# => 122
 # (What is the significance of the number 122 in relation to the character z?)
 
-# "How many times does the letter 'a' appear in this string?".<???>
+p "How many times does the letter 'a' appear in this string?".count("a")
 # => 4
