f581bb075dadcccf41622c9e1041e659165f7704
ikn02010 <ikn02010@gmail.com>
2017-08-20 07:54:23 -0700

    create puppy class and characteristics

---
+++ ruby/puppy.rb
@@ -0,0 +1,16 @@
+# #puppy class
+
+# SPECIES------------
+# Puppylashus
+
+# CHARACTERISTICS-----------
+# Name: varies
+# Color: varies
+# Eye count: two
+# Friendly: yes
+
+# BEHAVIOR--------------
+# Playful
+# Hungry
+# Affectionate
+# Needy
