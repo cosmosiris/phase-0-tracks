fb8431e49d71d5e59b7e239b009e389d84957969
ikn02010 <ikn02010@gmail.com>
2017-08-21 14:40:52 -0700

    complete 6.4 module

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,13 +1,41 @@
+#Old Code
+# module Shout
+#    def self.yell_angrily(words)
+#       words + "!!!" + " :("
+#    end
+
+#   def self.yell_happily(words)
+#     words + "!!!" + " :D"
+#   end
+# end
+
+# puts Shout.yell_angrily("oh my gosh, shutup")
+# puts Shout.yell_happily("Oh my gosh, I love you")
+
+#Mixin Version
+
 module Shout
-   def self.yell_angrily(words)
-      words + "!!!" + " :("
+   def yell_angrily(words)
+    words + "!!!" + " :("
    end
 
-  def self.yell_happily(words)
+  def yell_happily(words)
     words + "!!!" + " :D"
   end
 end
 
-puts Shout.yell_angrily("oh my gosh, shutup")
-puts Shout.yell_happily("Oh my gosh, I love you")
+class  Child
+  include Shout
+end
+
+class Mom
+  include Shout
+end
+
+child = Child.new
+puts child.yell_happily("Oh my god thank you mommy")
+mom = Mom.new
+puts mom.yell_angrily("Oh my god, why did you do that?")
+
+
 
