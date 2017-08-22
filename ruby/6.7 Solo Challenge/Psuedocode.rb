a9e9975272e338119552a005a64776a04ae45335
ikn02010 <ikn02010@gmail.com>
2017-08-22 11:58:05 -0700

    complete 6.7 solo challenge game class and driver code

--- ruby/6.7 Solo Challenge/Psuedocode.rb
+++ ruby/6.7 Solo Challenge/Psuedocode.rb
@@ -2,14 +2,14 @@
 
 # Define class
 class WordGuess
-  attr_reader :word, :guess_count
-  attr_accessor :guess
+  attr_reader :word, :is_over
+  attr_accessor :guess, :guess_array
 # Initialize and add instance variables
   def initialize
     @word = word
     @guess = guess
-    @guess_count = 0
     @guess_array =[]
+    @is_over = false
   end
 #method for one user to enter a word
   def set_word(word)
@@ -17,18 +17,24 @@ class WordGuess
   end
 #method for second user to guess the word
 #method to check the guess against the word
-#Check guess and limit guesses to length of the word, except repeat guesses do not count against the user.
+#Check guess and limit guesses to length of the word, except repeat guesses do not count against the user.User recieves a congratulatory message if they win and a taunting message if they lose
   def guess_check(guess)
     @guess = guess
     @guess_array << guess
-    @guess_count += 1
+    @guess_array.uniq!
     if @guess.downcase == @word.downcase
       puts "Congratulations, you are so smart"
+      @is_over = true
+    elsif guess_array.length == word.length
+      puts "Sorry, you lose"
+      @is_over = true
     else
-      puts "Wrong, you have #{word.length - @guess_count} attempts remaining"
+      puts "Wrong, you have #{word.length - @guess_array.length} attempts remaining"
+      #guessing player recieives continual feedback on current state of the word
+      puts "Here is a hint: the word has the letter #{@word[rand(word.length)]}"
+
     end
-    @guess_array.uniq!
-    puts @guess_array
+    puts "These are your guesses so far: #{@guess_array}"
   end
 end
 
@@ -42,17 +48,15 @@ word = gets.chomp
 game.set_word(word)
 
 #Loop
-while game.guess_count < word.length
+while !game.is_over
   #Enter guess
   puts "Enter the word you think user number 1 entered"
   guess = gets.chomp
 
   #Check Guess
   game.guess_check(guess)
-
 end
 
 
-#guessing player recieives continual feedback on current state of the word
 
-#User recieves a congratulatory message if they win and a taunting message if they lose
+
