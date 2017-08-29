6a14f9be98e85bf976d27cde12d6ba2c428180a3
ikn02010 <ikn02010@gmail.com>
2017-08-29 13:50:26 -0700

    create file, declare colors and horses variables, and push new strings to variables

---
+++ js/data_structures.js
@@ -0,0 +1,11 @@
+//declare variables
+var colors = ["blue", "red", "yellow", "pink"];
+
+var horses = ["Kitty", "Joe", "Bambi", "Simba"];
+
+//add to variables
+colors.push("gold");
+horses.push("Sam");
+
+console.log(colors);
+console.log(horses);
