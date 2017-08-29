d8bf6a1d006749f60070254974bb514816edcf90
ikn02010 <ikn02010@gmail.com>
2017-08-29 15:37:15 -0700

    complete release 2 and answer release 3 questions

--- js/data_structures.js
+++ js/data_structures.js
@@ -34,5 +34,31 @@ function Car(model, year, color, isUsed){
   this.year = year;
   this.color = color;
   this.isUsed = isUsed;
-  this.age = function(year){console.log("This car is " + 2017-year + " years old");};
+  this.age = function() {console.log("This car is " + (2017-year) + " years old"); };
 }
+
+var iris_car = new Car("Toyota", 2009, "white", false);
+console.log(iris_car);
+iris_car.age();
+
+var lina_car = new Car("Jeep", 2017, "grey", true)
+console.log(lina_car);
+lina_car.age();
+
+//Release 3:
+//how do you loop through the keys and values of an object?
+//We Could only find one method which is a for loop.
+var hash = {name: "iris", age: 40, gender: "female"};
+for (var key in hash){
+  console.log(key + " : " + hash[key]);
+}
+
+
+//Are there advantages to using constructor functions to create objects? Disadvantages?
+//the object constructor allows you to make new objects of the same type. It is faster and more efficient than making each object one at a time.
+  //1. Advantage: Constructors have better memory performance.
+  //2. Advantage: Less likely to make a mistake because they all follow the format of the constructor
+  //3. Disadvantage: you always have to construct a child of a pre-defined object. If you want to add a new parameter to just one object you must add it to the constructor first which impacts all objects made from it.
+  //Disadvantage: you might forget to type new
+
+
