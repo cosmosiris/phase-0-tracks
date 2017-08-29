da4a4d80ce91276c174e2ce61789ac074401c351
ikn02010 <ikn02010@gmail.com>
2017-08-29 09:44:58 -0700

    add driver code that prints variable if some condition is true

--- js/explore.js
+++ js/explore.js
@@ -3,11 +3,16 @@
 // add each letter of the string to a new variable
 
 function reverse(string){
-  new_string = ""
+  var new_string = "";
   for(var i = string.length-1; i >= 0; i-- ){
-  new_string += string[i]
+  new_string += string[i];
   }
-  console.log(new_string)
+  if (new_string.length > 5){
+    console.log(new_string);
+  }
+  else{
+    console.log("This string is too short!");
+  }
 }
 
 reverse("Sweetiepies")
