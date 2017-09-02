aacffe657c64040406e84384eaed32f2c27338a8
ikn02010 <ikn02010@gmail.com>
2017-09-01 20:00:14 -0700

    completed longest phrase sorting function

--- js/algos.js
+++ js/algos.js
@@ -1,17 +1,24 @@
 //7.3 Solo Challenge: Javascript Algorithms
 // Release 0: Find the Longest Phrase
 // write function that takes an array of strings and returns the longest phrase.
-function longest_phrase(array){
+
+var longest_phrase = function(array){
   //create loop that determines length of each string and compares it to the string next to it
-  var array_replace = ""
-  for(var i = 0; i < array.length; i++){
-    if (array[i].length > array[i+1].length){
-      array_replace = array[i];
-      array[i] = array[i+1];
-      array[i+1] = array_replace;
+  var sorted = false
+  while(sorted == false){
+    sorted = true;
+    //^^makes sure that the while loop above does not stop until the for loop below makes an entire iteration without changing the value of sorted to false. Once an entire iteration happens without changing the value of sorted to false, we know that the array has been sorted
+    for(var i = 0; i < array.length-1; i++){
+      if (array[i].length > array[i+1].length){
+        var phrase_to_switch = array[i];
+        array[i] = array[i+1];
+        array[i+1] = phrase_to_switch;
+        sorted = false;
+      }
     }
   }
-  console.log(array)
+  console.log(array[array.length-1]);
+  return (array[array.length-1])
 }
 
-longest_phrase(["Iris", "Kathleen", "Nevins"]);
+longest_phrase(["Iris", "Kathleen", "Nevins", "Harold", "Andrea", "Dean"]);
