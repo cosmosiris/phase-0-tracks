ab1f909aaed8714c36062a9e4ac9db4bd4cfadf2
ikn02010 <ikn02010@gmail.com>
2017-09-09 13:54:57 -0700

    Create files

---
+++ databases/accountability_tool.rb
@@ -0,0 +1,18 @@
+# OPERATION ACCOUNTABILITY!
+
+# require gems
+require "sqlite3"
+require "faker"
+
+# create SQlite3 database
+db = SQLite3::Database.new("project_planning.db")
+
+# learn about fancy string delimiters
+
+# create a project table
+
+# add a test project
+
+# explore ORM by retrieving database
+
+# add lots of projects
